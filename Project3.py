2adf7275fbc24315dffcce3526da8c0ade2d4347
Mzrcin47 <marcin.pawluch@torontomu.ca>
2023-12-04 17:14:44 -0500

    Adjusted Thresholding

--- Project3.py
+++ Project3.py
@@ -7,32 +7,31 @@ from torchvision import transforms
 image_path = 'data/motherboard_image.JPEG'
 original_image = cv2.imread(image_path)
 
-
 original_image = cv2.resize(original_image, (800, 600))
-
 
 gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
-_, thresholded_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
-edges = cv2.Canny(thresholded_image, 30, 100)
-contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Adjusted thresholding
+_, thresholded_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Dilate the image
+dilated_image = cv2.dilate(thresholded_image, None, iterations=2)
+
+# Find contours again
+contours, _ = cv2.findContours(dilated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 min_contour_area = 500
 filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
 
 mask = np.zeros_like(gray_image, dtype=np.uint8)
 
-if filtered_contours:
-
-    largest_contour = max(filtered_contours, key=cv2.contourArea)
-    cv2.drawContours(mask, [largest_contour], -1, (255), thickness=cv2.FILLED)
-
+# Draw all filtered contours on the mask
+cv2.drawContours(mask, filtered_contours, -1, (255), thickness=cv2.FILLED)
 
 original_image = original_image.astype(np.uint8)
 mask = mask.astype(np.uint8)
 
 result = cv2.bitwise_and(original_image, original_image, mask=mask)
-
 
 cv2.imshow('Original Image', cv2.resize(original_image, (800, 600)))
 cv2.imshow('Mask', cv2.resize(mask, (800, 600)))
